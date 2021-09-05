bb30a521cc569fcbb04d989e9cf28c2c58094156
Khatya Khakhutaishvili <kkhatya@gmail.com>
2021-09-05 15:18:29 +0300

    feature: post

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -6,10 +6,10 @@ export default ( { id, title, score, by, time }) => {
   return (
     <li className="post">
       <Link to={`/posts/${id}`} className="post__link">
-        <p className="post__title">{title}</p>
         <span className="post__score">{score}</span>
         <div className="post__description">
           <span className="post__date">{time}</span>
+          <h3 className="post__title">{title}</h3>
           <span className="post__autor">{by}</span>
         </div>
       </Link>
